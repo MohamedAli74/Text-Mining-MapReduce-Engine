1efbaa2b395c0a87f40a453ab51d353a88e9f419
MohamedAli74 <mohamedali243.123@gmail.com>
2025-12-11 00:10:29 +0200

    - Created collocation class for key-value pairs in MapReduce

---
+++ dsp2/src/writables/collocation.java
@@ -0,0 +1,66 @@
+package writables;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+
+public class collocation implements WritableComparable<collocation>{
+    
+    private Text decade;//TODO: might need to change it
+    private Text word1;
+    private Text word2;
+
+    public collocation(){
+        decade = new Text();
+        word1 = new Text();
+        word2 = new Text();
+    }
+
+    public collocation(Text decade, Text word1, Text word2){
+        this.decade = decade;
+        this.word1 = word1;
+        this.word2 = word2;
+    }
+    public void set(Text decade, Text word1, Text word2){
+        this.decade = decade;
+        this.word1 = word1;
+        this.word2 = word2;
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        decade = in.readUTF();                
+        word1 = in.readUTF();                
+        word2 = in.readUTF();                
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeUTF(decade);
+        out.writeUTF(word1);
+        out.writeUTF(word2);     
+    }
+
+    @Override
+    public int compareTo(DecadeKey other) {
+        int cmp = this.decade.compareTo(other.decade);
+        if (cmp != 0) {
+            return cmp;
+        }
+        
+        cmp = this.word1.compareTo(other.word1);
+        if (cmp != 0) {
+            return cmp;
+        }
+        
+        return this.word2.compareTo(other.word2);
+    }
+    
+    // 4.for the Reducer to read the data
+    public Text getDecade() { return decade; }
+    public Text getWord1()  { return word1; }
+    public Text getWord2()  { return word2; }
+
+}
