9b53cb81c5af83ec3a7136f2ebefc57ef1adbacd
MohamedAli74 <mohamedali243.123@gmail.com>
2025-12-12 01:00:14 +0200

    added job4 implementation with mapper, reducer, partitioner and grouping comparator

---
+++ dsp2/src/main/java/dsp2/job4.java
@@ -0,0 +1,80 @@
+package dsp2;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import writables.DoublePair;
+import writables.collocation;
+
+public class job4 {
+    /**
+     * @input key -> (decade, word1, word2), value -> "Rario"
+     * @output key -> (decade,ratio), value -> "word1,word2"
+     */
+    public static class job4Mapper extends Mapper<collocation, DoubleWritable, DoublePair, Text>{
+        @Override
+        protected void map(collocation key ,DoubleWritable ratio , Context context)
+            throws IOException,
+            InterruptedException {
+                DoublePair outKey = new DoublePair(key.getDecade(),ratio);
+                Text outValue = new Text(key.getWord1().toString() +","+ key.getWord2().toString());
+                context.write(outKey, outValue); 
+        }
+    }
+
+    public static class Job4Partitioner extends Partitioner<DoublePair, Text>{
+        
+        @Override
+        public int getPartition(DoublePair key, Text value, int numPartitions) {
+            //only hash 'decade' to ensure all the collocations from the same decade gets to the same reducer.
+            int hash = key.getDecade().toString().hashCode();
+            return (hash & Integer.MAX_VALUE) % numPartitions;
+        }
+    }
+
+    public static class DecadeGroupingComparator extends WritableComparator {//TODO: use this in the main
+        
+        // 1. Constructor: Tells Hadoop to convert byte streams into 'DoublePair' objects
+        //    so we can compare them easily.
+        protected DecadeGroupingComparator() {
+            super(DoublePair.class, true);
+        }
+
+        @Override
+        public int compare(WritableComparable a, WritableComparable b) {
+            DoublePair p1 = (DoublePair) a;
+            DoublePair p2 = (DoublePair) b;
+            
+            //Compare ONLY the decade. Ignore the ratio
+            return p1.getDecade().compareTo(p2.getDecade());
+        }
+    }
+
+    public static class Job4Reducer extends Reducer<DoublePair, Text, DoublePair, Text>{        
+        @Override
+        // Running EXACTLY ONCE per decade
+        public void reduce(DoublePair key, Iterable<Text> values, Context context)
+            throws IOException,
+            InterruptedException {
+            
+            int counter = 0;
+
+            for (Text val : values) {
+                if (counter < 100) {
+                context.write(key, val);
+                    counter++;
+                } else {
+                    return; 
+                }
+            }
+        }
+    }
+}
